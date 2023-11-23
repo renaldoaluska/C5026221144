cb1d38b0832b39c1f421ad0c38ec7e0e776b7450
renaldoaluska <alfaaluska@gmail.com>
2023-11-23 14:53:43 +0700

    pertemuan 23 nov 2023

--- routes/web.php
+++ routes/web.php
@@ -119,3 +119,11 @@ Route::get('/tentang', function () {
 Route::get('/kontak', function () {
     return view('kontak');
 });
+
+//route CRUD
+Route::get('/pegawai','App\Http\Controllers\PegawaiController@index');
+Route::get('/pegawai/tambah','App\Http\Controllers\PegawaiController@tambah');
+Route::post('/pegawai/store','App\Http\Controllers\PegawaiController@store');
+Route::get('/pegawai/edit/{id}','App\Http\Controllers\PegawaiController@edit');
+Route::post('/pegawai/update','App\Http\Controllers\PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','App\Http\Controllers\PegawaiController@hapus');
